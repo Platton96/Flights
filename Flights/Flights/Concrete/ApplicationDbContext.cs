ada90aa34a340c543349e7ff58bb3c1870a78a4a
Kulbeda Sergey <sergey_kulbeda@mail.ru>
2017-11-07 12:02:58 +0300

    create applicationUser

---
+++ Flights/Flights/Concrete/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Flights.Entity;
+namespace Flights.Concrete
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
